70464b5beedad99086e408b5b165a5e1cb85c375
Nicolas Le <156036222+KyuuT3A@users.noreply.github.com>
2024-11-17 04:31:08 -0500

    Update FlyMe2theMoon(6).sql

--- FlyMe2theMoon(6).sql
+++ FlyMe2theMoon(6).sql
@@ -14,21 +14,21 @@ SET NOCOUNT ON;
 -- --------------------------------------------------------------------------------
 
 -- Drop Table Statements
-IF OBJECT_ID ('TPilotFlights')			IS NOT NULL DROP TABLE TPilotFlights
-IF OBJECT_ID ('TAttendantFlights')		IS NOT NULL DROP TABLE TAttendantFlights
-IF OBJECT_ID ('TFlightPassengers')		IS NOT NULL DROP TABLE TFlightPassengers
-IF OBJECT_ID ('TMaintenanceMaintenanceWorkers')			IS NOT NULL DROP TABLE TMaintenanceMaintenanceWorkers
+IF OBJECT_ID ('TPilotFlights')				IS NOT NULL DROP TABLE TPilotFlights
+IF OBJECT_ID ('TAttendantFlights')			IS NOT NULL DROP TABLE TAttendantFlights
+IF OBJECT_ID ('TFlightPassengers')			IS NOT NULL DROP TABLE TFlightPassengers
+IF OBJECT_ID ('TMaintenanceMaintenanceWorkers')		IS NOT NULL DROP TABLE TMaintenanceMaintenanceWorkers
 
-IF OBJECT_ID ('TPassengers')			IS NOT NULL DROP TABLE TPassengers
+IF OBJECT_ID ('TPassengers')				IS NOT NULL DROP TABLE TPassengers
 IF OBJECT_ID ('TPilots')				IS NOT NULL DROP TABLE TPilots
-IF OBJECT_ID ('TAttendants')			IS NOT NULL DROP TABLE TAttendants
-IF OBJECT_ID ('TMaintenanceWorkers')	IS NOT NULL DROP TABLE TMaintenanceWorkers
+IF OBJECT_ID ('TAttendants')				IS NOT NULL DROP TABLE TAttendants
+IF OBJECT_ID ('TMaintenanceWorkers')			IS NOT NULL DROP TABLE TMaintenanceWorkers
 IF OBJECT_ID('TEmployees')				IS NOT NULL DROP TABLE TEmployees;
 IF OBJECT_ID ('TFlights')				IS NOT NULL DROP TABLE TFlights
-IF OBJECT_ID ('TMaintenances')			IS NOT NULL DROP TABLE TMaintenances
+IF OBJECT_ID ('TMaintenances')				IS NOT NULL DROP TABLE TMaintenances
 IF OBJECT_ID ('TPlanes')				IS NOT NULL DROP TABLE TPlanes
-IF OBJECT_ID ('TPlaneTypes')			IS NOT NULL DROP TABLE TPlaneTypes
-IF OBJECT_ID ('TPilotRoles')			IS NOT NULL DROP TABLE TPilotRoles
+IF OBJECT_ID ('TPlaneTypes')				IS NOT NULL DROP TABLE TPlaneTypes
+IF OBJECT_ID ('TPilotRoles')				IS NOT NULL DROP TABLE TPilotRoles
 IF OBJECT_ID ('TAirports')				IS NOT NULL DROP TABLE TAirports
 IF OBJECT_ID ('TStates')				IS NOT NULL DROP TABLE TStates
 -- --------------------------------------------------------------------------------
@@ -36,14 +36,14 @@ IF OBJECT_ID ('TStates')				IS NOT NULL DROP TABLE TStates
 -- --------------------------------------------------------------------------------
 CREATE TABLE TPassengers
 (
-	 intPassengerID			INTEGER			NOT NULL
-	,strFirstName			VARCHAR(255)	NOT NULL
-	,strLastName			VARCHAR(255)	NOT NULL
+	 intPassengerID				INTEGER			NOT NULL
+	,strFirstName				VARCHAR(255)	NOT NULL
+	,strLastName				VARCHAR(255)	NOT NULL
 	,strAddress				VARCHAR(255)	NOT NULL
 	,strCity				VARCHAR(255)	NOT NULL
 	,intStateID				INTEGER			NOT NULL
 	,strZip					VARCHAR(255)	NOT NULL
-	,strPhoneNumber			VARCHAR(255)	NOT NULL
+	,strPhoneNumber				VARCHAR(255)	NOT NULL
 	,strEmail				VARCHAR(255)	NOT NULL
 	,strPassengerLoginID	VARCHAR(255)	NOT NULL
 	,strPassengerPassword	VARCHAR(255)	NOT NULL
@@ -54,105 +54,105 @@ CREATE TABLE TPassengers
 CREATE TABLE TPilots
 (
 	 intPilotID				INTEGER			NOT NULL
-	,strFirstName			VARCHAR(255)	NOT NULL
-	,strLastName			VARCHAR(255)	NOT NULL
-	,strEmployeeID			VARCHAR(255)	NOT NULL
-	,dtmDateOfHire			DATETIME		NOT NULL
-	,dtmDateOfTermination	DATETIME		NOT NULL
-	,dtmDateOfLicense		DATETIME		NOT NULL
-	,intPilotRoleID			INTEGER			NOT NULL
+	,strFirstName				VARCHAR(255)	NOT NULL
+	,strLastName				VARCHAR(255)	NOT NULL
+	,strEmployeeID				VARCHAR(255)	NOT NULL
+	,dtmDateOfHire				DATETIME		NOT NULL
+	,dtmDateOfTermination			DATETIME		NOT NULL
+	,dtmDateOfLicense			DATETIME		NOT NULL
+	,intPilotRoleID				INTEGER			NOT NULL
 
 	,CONSTRAINT TPilots_PK PRIMARY KEY ( intPilotID )
 )
 
 CREATE TABLE TAttendants
 (
-	 intAttendantID			INTEGER			NOT NULL
+	 intAttendantID			INTEGER		NOT NULL
 	,strFirstName			VARCHAR(255)	NOT NULL
 	,strLastName			VARCHAR(255)	NOT NULL
 	,strEmployeeID			VARCHAR(255)	NOT NULL
-	,dtmDateOfHire			DATETIME		NOT NULL
-	,dtmDateOfTermination	DATETIME		NOT NULL
+	,dtmDateOfHire			DATETIME	NOT NULL
+	,dtmDateOfTermination		DATETIME	NOT NULL
 	,CONSTRAINT TAttendants_PK PRIMARY KEY ( intAttendantID )
 )
 
 CREATE TABLE TMaintenanceWorkers
 (
-	 intMaintenanceWorkerID	INTEGER			NOT NULL
+	 intMaintenanceWorkerID		INTEGER		NOT NULL
 	,strFirstName			VARCHAR(255)	NOT NULL
 	,strLastName			VARCHAR(255)	NOT NULL
 	,strEmployeeID			VARCHAR(255)	NOT NULL
-	,dtmDateOfHire			DATETIME		NOT NULL
-	,dtmDateOfTermination	DATETIME		NOT NULL
-	,dtmDateOfCertification	DATETIME		NOT NULL
+	,dtmDateOfHire			DATETIME	NOT NULL
+	,dtmDateOfTermination		DATETIME	NOT NULL
+	,dtmDateOfCertification		DATETIME	NOT NULL
 	,CONSTRAINT TMaintenanceWorkers_PK PRIMARY KEY ( intMaintenanceWorkerID )
 )
 
 CREATE TABLE TStates
 (
-	 intStateID			INTEGER			NOT NULL
+	 intStateID			INTEGER		NOT NULL
 	,strState			VARCHAR(255)	NOT NULL
 	,CONSTRAINT TStates_PK PRIMARY KEY ( intStateID )
 )
 
 CREATE TABLE TFlights
 (
-	 intFlightID			INTEGER			NOT NULL
+	 intFlightID			INTEGER		NOT NULL
 	,strFlightNumber		VARCHAR(255)	NOT NULL
-	,dtmFlightDate			DATETIME		NOT NULL
-	,dtmTimeofDeparture		TIME			NOT NULL
-	,dtmTimeOfLanding		TIME			NOT NULL
-	,intFromAirportID		INTEGER			NOT NULL
-	,intToAirportID			INTEGER			NOT NULL
-	,intMilesFlown			INTEGER			NOT NULL
-	,intPlaneID				INTEGER			NOT NULL
+	,dtmFlightDate			DATETIME	NOT NULL
+	,dtmTimeofDeparture		TIME		NOT NULL
+	,dtmTimeOfLanding		TIME		NOT NULL
+	,intFromAirportID		INTEGER		NOT NULL
+	,intToAirportID			INTEGER		NOT NULL
+	,intMilesFlown			INTEGER		NOT NULL
+	,intPlaneID			INTEGER		NOT NULL
 	,CONSTRAINT TFlights_PK PRIMARY KEY ( intFlightID )
 )
 
 CREATE TABLE TMaintenances
 (
-	 intMaintenanceID		INTEGER			NOT NULL
+	 intMaintenanceID		INTEGER		NOT NULL
 	,strWorkCompleted		VARCHAR(8000)	NOT NULL
-	,dtmMaintenanceDate		DATETIME		NOT NULL
-	,intPlaneID				INTEGER			NOT NULL
+	,dtmMaintenanceDate		DATETIME	NOT NULL
+	,intPlaneID			INTEGER		NOT NULL
 	,CONSTRAINT TMaintenances_PK PRIMARY KEY ( intMaintenanceID )
 )
 
 CREATE TABLE TPlanes
 (
-	 intPlaneID				INTEGER			NOT NULL
+	 intPlaneID			INTEGER		NOT NULL
 	,strPlaneNumber			VARCHAR(255)	NOT NULL
-	,intPlaneTypeID			INTEGER			NOT NULL
+	,intPlaneTypeID			INTEGER		NOT NULL
 	,CONSTRAINT TPlanes_PK PRIMARY KEY ( intPlaneID )
 )
 
 CREATE TABLE TEmployees 
 (
-    intEmployeeID				INTEGER			NOT NULL
+    intEmployeeID			INTEGER		NOT NULL
    ,strEmployeeLoginID			VARCHAR(255)	NOT NULL
    ,strEmployeePassword			VARCHAR(255)	NOT NULL
-   ,strEmployeeRole				VARCHAR(255)	NOT NULL
-   ,EmployeeID					INTEGER
+   ,strEmployeeRole			VARCHAR(255)	NOT NULL
+   ,EmployeeID				INTEGER		NOT NULL
    ,CONSTRAINT TEmployees_PK PRIMARY KEY ( intEmployeeID ) 
 );
 
 CREATE TABLE TPlaneTypes	
 (
-	 intPlaneTypeID			INTEGER			NOT NULL
+	 intPlaneTypeID			INTEGER		NOT NULL
 	,strPlaneType			VARCHAR(255)	NOT NULL
 	,CONSTRAINT TPlaneTypes_PK PRIMARY KEY ( intPlaneTypeID )
 )
 
 CREATE TABLE TPilotRoles	
 (
-	 intPilotRoleID			INTEGER			NOT NULL
+	 intPilotRoleID			INTEGER		NOT NULL
 	,strPilotRole			VARCHAR(255)	NOT NULL
 	,CONSTRAINT TPilotRoles_PK PRIMARY KEY ( intPilotRoleID )
 )
 
 CREATE TABLE TAirports
 (
-	 intAirportID			INTEGER			NOT NULL
+	 intAirportID			INTEGER		NOT NULL
 	,strAirportCity			VARCHAR(255)	NOT NULL
 	,strAirportCode			VARCHAR(255)	NOT NULL
 	,CONSTRAINT TAirports_PK PRIMARY KEY ( intAirportID )
@@ -161,7 +161,7 @@ CREATE TABLE TAirports
 CREATE TABLE TPilotFlights
 (
 	 intPilotFlightID		INTEGER			NOT NULL
-	,intPilotID				INTEGER			NOT NULL
+	,intPilotID			INTEGER			NOT NULL
 	,intFlightID			INTEGER			NOT NULL
 	,CONSTRAINT TPilotFlights_PK PRIMARY KEY ( intPilotFlightID )
 )
@@ -176,20 +176,20 @@ CREATE TABLE TAttendantFlights
 
 CREATE TABLE TFlightPassengers
 (
-	 intFlightPassengerID		INTEGER			NOT NULL
+	 intFlightPassengerID			INTEGER			NOT NULL
 	,intFlightID				INTEGER			NOT NULL
 	,intPassengerID				INTEGER			NOT NULL
-	,strSeat					VARCHAR(255)	NOT NULL
+	,strSeat				VARCHAR(255)		NOT NULL
 	,intFlightCost				INTEGER			NOT NULL
 	,CONSTRAINT TFlightPassengers_PK PRIMARY KEY ( intFlightPassengerID )
 )
 
 CREATE TABLE TMaintenanceMaintenanceWorkers
 (
-	 intMaintenanceMaintenanceWorkerID		INTEGER			NOT NULL
-	,intMaintenanceID						INTEGER			NOT NULL
-	,intMaintenanceWorkerID					INTEGER			NOT NULL
-	,intHours								INTEGER			NOT NULL
+	 intMaintenanceMaintenanceWorkerID		INTEGER		NOT NULL
+	,intMaintenanceID				INTEGER		NOT NULL
+	,intMaintenanceWorkerID				INTEGER			NOT NULL
+	,intHours					INTEGER			NOT NULL
 	,CONSTRAINT TMaintenanceMaintenanceWorkers_PK PRIMARY KEY ( intMaintenanceMaintenanceWorkerID )
 )
 
@@ -200,20 +200,20 @@ CREATE TABLE TMaintenanceMaintenanceWorkers
 -- #	Child							Parent						Column
 -- -	-----							------						---------
 -- 1	TPassengers						TStates						intStateID	
--- 2	TFlightPassenger				TPassengers					intPassengerID
+-- 2	TFlightPassenger					TPassengers					intPassengerID
 -- 3	TFlights						TPlanes						intPlaneID
 -- 4	TFlights						TAirports					intFromAiportID
 -- 5	TFlights						TAirports					intToAiportID
--- 6	TPilotFlights					TFlights					intFlightID
--- 7	TAttendantFlights				TFlights					intFlightID
--- 8	TPilotFlights					TPilots						intPilotID
--- 9	TAttendantFlights				TAttendants					intAttendantID
+-- 6	TPilotFlights						TFlights					intFlightID
+-- 7	TAttendantFlights					TFlights					intFlightID
+-- 8	TPilotFlights						TPilots						intPilotID
+-- 9	TAttendantFlights					TAttendants					intAttendantID
 -- 10	TPilots							TPilotRoles					intPilotRoleID
 -- 11	TPlanes							TPlaneTypes					intPlaneTypeID
--- 12	TMaintenances					TPlanes						intPlaneID
--- 13	TMaintenanceMaintenanceWorkers	TMaintenance				intMaintenanceID
--- 14	TMaintenanceMaintenanceWorkers	TMaintenanceWorker			intMaintenanceWorkerID
--- 15	TFlightPassenger				TFlights					intFlightID
+-- 12	TMaintenances						TPlanes						intPlaneID
+-- 13	TMaintenanceMaintenanceWorkers				TMaintenance					intMaintenanceID
+-- 14	TMaintenanceMaintenanceWorkers				TMaintenanceWorker				intMaintenanceWorkerID
+-- 15	TFlightPassenger					TFlights					intFlightID
 
 
 
@@ -278,21 +278,21 @@ FOREIGN KEY ( intFlightID ) REFERENCES TFlights (intFlightID )
 --	Step #3 : Add Data - INSERTS
 -- --------------------------------------------------------------------------------
 INSERT INTO TStates( intStateID, strState)
-VALUES				(1, 'Ohio')
+VALUES				   (1, 'Ohio')
 				   ,(2, 'Kentucky')
 				   ,(3, 'Indiana')
 
 INSERT INTO TPilotRoles( intPilotRoleID, strPilotRole)
-VALUES				(1, 'Co-Pilot')
+VALUES				   (1, 'Co-Pilot')
 				   ,(2, 'Captain')
 				   				
 INSERT INTO TPlaneTypes( intPlaneTypeID, strPlaneType)
-VALUES				(1, 'Airbus A350')
+VALUES				   (1, 'Airbus A350')
 				   ,(2, 'Boeing 747-8')
 				   ,(3, 'Boeing 767-300F')
 				   
 INSERT INTO TPlanes( intPlaneID, strPlaneNumber, intPlaneTypeID)
-VALUES				(1, '4X887G', 1)
+VALUES				   (1, '4X887G', 1)
 				   ,(2, '5HT78F', 2)
 				   ,(3, '5TYY65', 2)
 				   ,(4, '4UR522', 1)
@@ -300,7 +300,7 @@ VALUES				(1, '4X887G', 1)
 				   ,(6, '67TYHH', 3)
 				   
 INSERT INTO TAirports( intAirportID, strAirportCity, strAirportCode)
-VALUES				(1, 'Cincinnati', 'CVG')
+VALUES				   (1, 'Cincinnati', 'CVG')
 				   ,(2, 'Miami', 'MIA')
 				   ,(3, 'Ft. Meyer', 'RSW')
 				   ,(4, 'Louisville',  'SDF')
@@ -308,7 +308,7 @@ VALUES				(1, 'Cincinnati', 'CVG')
 				   ,(6, 'Orlando', 'MCO' )
 				   
 INSERT INTO TPassengers (intPassengerID, strFirstName, strLastName, strAddress, strCity, intStateID, strZip, strPhoneNumber, strEmail, strPassengerLoginID, strPassengerPassword, dtmPassengerDateofBirth)
-VALUES				  (1, 'Knelly', 'Nervious', '321 Elm St.', 'Cincinnati', 1, '45201', '5135553333', 'nnelly@gmail.com', 'Knelly321', 'Cinc45201', '1/1/2022')
+VALUES				     	 (1, 'Knelly', 'Nervious', '321 Elm St.', 'Cincinnati', 1, '45201', '5135553333', 'nnelly@gmail.com', 'Knelly321', 'Cinc45201', '1/1/2022')
 					 ,(2, 'Orville', 'Waite', '987 Oak St.', 'Cleveland', 1, '45218', '5135556333', 'owright@gmail.com', 'Orville987', 'Clev45218', '2/2/2000')
 					 ,(3, 'Eileen', 'Awnewe', '1569 Windisch Rd.', 'Dayton', 1, '45069', '5135555333', 'eonewe1@yahoo.com', 'Eileen1569', 'Dayt45069', '3/3/1967')
 					 ,(4, 'Bob', 'Eninocean', '44561 Oak Ave.', 'Florence', 2, '45246', '8596663333', 'bobenocean@gmail.com', 'Bob44561', 'Flor45246', '4/4/1958')
@@ -316,7 +316,7 @@ VALUES				  (1, 'Knelly', 'Nervious', '321 Elm St.', 'Cincinnati', 1, '45201', '
 					 ,(6, 'Kay', 'Oss', '4484 Bushfield Ave.', 'Lawrenceburg', 3, '45546', '2825553333', 'wehavekayoss@gmail.com', 'Kay4484', 'Lawr45546', '6/6/1996')
 					 					 
 INSERT INTO TEmployees (intEmployeeID, strEmployeeLoginID, strEmployeePassword, strEmployeeRole, EmployeeID)
-VALUES				 (1, 'john.doe', 'password123', 'Admin', 1)
+VALUES				 	(1, 'john.doe', 'password123', 'Admin', 1)
 					,(2, 'jane.smith', 'pass456', 'Pilot', 2)
 					,(3, 'alice.wonderland', 'abc123', 'Attendant', 3)
 					,(4, 'bob.builder', 'b0bPwd', 'Attendant', 3)
@@ -325,27 +325,27 @@ VALUES				 (1, 'john.doe', 'password123', 'Admin', 1)
 
 
 INSERT INTO TPilots (intPilotID, strFirstName, strLastName, strEmployeeID, dtmDateofHire, dtmDateofTermination, dtmDateofLicense, intPilotRoleID)
-VALUES				  (1, 'Tip', 'Seenow', '12121', '1/1/2015', '1/1/2099', '12/1/2014', 1)
+VALUES				  	(1, 'Tip', 'Seenow', '12121', '1/1/2015', '1/1/2099', '12/1/2014', 1)
 					 ,(2, 'Ima', 'Soring', '13322', '1/1/2016', '1/1/2099', '12/1/2015', 1)
 					 ,(3, 'Hugh', 'Encharge', '16666', '1/1/2017', '1/1/2099', '12/1/2016', 2)
 					 ,(4, 'Iwanna', 'Knapp', '17676', '1/1/2014', '1/1/2015', '12/1/2013', 1)
 					 ,(5, 'Rose', 'Ennair', '19909', '1/1/2012', '1/1/2099', '12/1/2011', 2)
 					 
 INSERT INTO TAttendants (intAttendantID, strFirstName, strLastName, strEmployeeID, dtmDateofHire, dtmDateofTermination)
-VALUES				  (1, 'Miller', 'Tyme', '22121', '1/1/2015', '1/1/2099')
+VALUES				 	 (1, 'Miller', 'Tyme', '22121', '1/1/2015', '1/1/2099')
 					 ,(2, 'Sherley', 'Ujest', '23322', '1/1/2016', '1/1/2099')
 					 ,(3, 'Buhh', 'Biy', '26666', '1/1/2017', '1/1/2099')
 					 ,(4, 'Myles', 'Amanie', '27676', '1/1/2014', '1/1/2015')
 					 ,(5, 'Walker', 'Toexet', '29909', '1/1/2012', '1/1/2099')
 					 
 INSERT INTO TMaintenanceWorkers (intMaintenanceWorkerID, strFirstName, strLastName, strEmployeeID, dtmDateofHire, dtmDateofTermination, dtmDateOfCertification)
-VALUES				  (1, 'Gressy', 'Nuckles', '32121', '1/1/2015', '1/1/2099', '12/1/2014')
+VALUES				 	 (1, 'Gressy', 'Nuckles', '32121', '1/1/2015', '1/1/2099', '12/1/2014')
 					 ,(2, 'Bolt', 'Izamiss', '33322', '1/1/2016', '1/1/2099', '12/1/2015')
 					 ,(3, 'Sharon', 'Urphood', '36666', '1/1/2017', '1/1/2099','12/1/2016')
 					 ,(4, 'Ides', 'Racrozed', '37676', '1/1/2014', '1/1/2015','12/1/2013')
 					
 INSERT INTO TMaintenances (intMaintenanceID, strWorkCompleted, dtmMaintenanceDate, intPlaneID)
-VALUES				  (1, 'Fixed Wing', '1/1/2022', 1)
+VALUES					  (1, 'Fixed Wing', '1/1/2022', 1)
 					 ,(2, 'Repaired Flat Tire', '3/1/2022', 2)
 					 ,(3, 'Added Wiper Fluid', '4/1/2022', 3)
 					 ,(4, 'Tightened Engine to Wing', '5/1/2022', 2)
@@ -354,7 +354,7 @@ VALUES				  (1, 'Fixed Wing', '1/1/2022', 1)
 					 ,(7, 'Trapped Raccoon in Cargo Hold', '5/1/2022', 6)
 					 
 INSERT INTO TFlights (intFlightID, dtmFlightDate, strFlightNumber,  dtmTimeofDeparture, dtmTimeOfLanding, intFromAirportID, intToAirportID, intMilesFlown, intPlaneID)
-VALUES				  (1, '4/1/2022', '111', '10:00:00', '12:00:00', 1, 2, 1200, 2)
+VALUES				 	 (1, '4/1/2022', '111', '10:00:00', '12:00:00', 1, 2, 1200, 2)
 					 ,(2, '4/4/2022', '222','13:00:00', '15:00:00', 1, 3, 1000, 2)
 					 ,(3, '4/5/2022', '333','15:00:00', '17:00:00', 1, 5, 1200, 3)
 					 ,(4, '4/16/2022','444', '10:00:00', '12:00:00', 4, 6, 1100, 3)
@@ -365,19 +365,19 @@ VALUES				  (1, '4/1/2022', '111', '10:00:00', '12:00:00', 1, 2, 1200, 2)
 					 ,(9, '4/19/2022', '999','08:00:00', '10:00:00', 4, 2, 1000, 6)
 					 ,(10, '4/22/2022', '091','10:00:00', '12:00:00', 2, 1, 1200, 6)
 					 ,(11, '04/10/2024', 'Delta', '08:00:00', '10:00:00', 1, 2, 1600, 1)
-				     ,(12, '07/15/2024', 'Frontier', '09:30:00', '14:00:00', 2, 3, 700, 2) 
+				    	 ,(12, '07/15/2024', 'Frontier', '09:30:00', '14:00:00', 2, 3, 700, 2) 
 					 ,(13, '09/01/2024', 'Southwest ', '10:45:00', '10:00:00', 2, 4, 500, 1)
-				     ,(14, '11/30/2024', 'United', '12:15:00', '14:00:00', 2, 6, 1600, 2)
+				    	 ,(14, '11/30/2024', 'United', '12:15:00', '14:00:00', 2, 6, 1600, 2)
 					 ,(15, '09/01/2026', 'Korean ', '11:45:12', '05:00:00', 3, 1, 1800, 1)
-				     ,(16, '11/30/2026', 'Korean', '12:20:00', '16:00:00', 4, 6, 1600, 2)
+				    	 ,(16, '11/30/2026', 'Korean', '12:20:00', '16:00:00', 4, 6, 1600, 2)
 					 ,(17, '08/12/2025', 'Frontier', '12:45:10', '18:20:11', 3, 1, 1440, 1) 
 					 ,(18, '02/01/2025', 'Southwest ', '11:21:16', '10:00:00', 2, 4, 1234, 2)
-				     ,(19, '05/30/2027', 'United', '08:11:26', '14:54:56', 1, 4, 2100, 2)
+				    	 ,(19, '05/30/2027', 'United', '08:11:26', '14:54:56', 1, 4, 2100, 2)
 					 ,(20, '06/01/2028', 'Korean ', '07:16:34', '05:23:54', 2, 6, 1180, 2)
-				     ,(21, '10/30/2026', 'Korean', '09:22:21', '16:12:34', 2, 1, 1750, 1);
+				    	 ,(21, '10/30/2026', 'Korean', '09:22:21', '16:12:34', 2, 1, 1750, 1);
 
 INSERT INTO TPilotFlights ( intPilotFlightID, intPilotID, intFlightID)
-VALUES				 ( 1, 1, 2 )
+VALUES					 ( 1, 1, 2 )
 					,( 2, 1, 3 )
 					,( 3, 3, 3 )
 					,( 4, 3, 2 )
@@ -401,7 +401,7 @@ VALUES				 ( 1, 1, 2 )
 					,( 22, 3, 16 )
 					
 INSERT INTO TAttendantFlights ( intAttendantFlightID, intAttendantID, intFlightID)
-VALUES				( 1, 1, 2 )
+VALUES					( 1, 1, 2 )
 					,( 2, 2, 3 )
 					,( 3, 3, 3 )
 					,( 4, 4, 2 )
@@ -425,7 +425,7 @@ VALUES				( 1, 1, 2 )
 					,( 22, 3, 16 )
 
 INSERT INTO TFlightPassengers ( intFlightPassengerID, intFlightID, intPassengerID, strSeat, intFlightCost)
-VALUES				 ( 1, 1, 1, '1A', 2350)
+VALUES					 ( 1, 1, 1, '1A', 2350)
 					,( 2, 1, 2, '2A',  3100)
 					,( 3, 1, 3, '1B',  1500)
 					,( 4, 1, 4, '1C',  3750)
@@ -455,7 +455,7 @@ VALUES				 ( 1, 1, 1, '1A', 2350)
 					,( 28, 15, 4, '2C', 2100)
 
 INSERT INTO TMaintenanceMaintenanceWorkers ( intMaintenanceMaintenanceWorkerID, intMaintenanceID, intMaintenanceWorkerID, intHours)
-VALUES				 ( 1, 2, 1, 2 )
+VALUES					 ( 1, 2, 1, 2 )
 					,( 2, 4, 1, 3 )
 					,( 3, 2, 3, 4 )
 					,( 4, 1, 4, 2 )
